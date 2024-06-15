af20ec81eabcdcb51e276f7b7fba53da168321db
Dustin Moroz <dustin.moroz@gmail.com>
2024-06-15 09:57:04 -0600

    Debugged average.js and reverse.js. Both files have been linted.

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
-var input = process.argv[2];
+const input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
+let reverse = function(original) {
+  return original.split('').reverse().join('');
+};
